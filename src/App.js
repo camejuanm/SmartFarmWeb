7921901c29472dca1b748edf41ba5d691cca3457
thenardhis2021 <thenardhi2001@gmail.com>
2022-10-28 08:23:03 +0700

    mengubah sidebar dan appjs

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SignUp from "./components/signup_component";
 import UserDetails from "./components/userDetails";
 import Reset from "./components/reset";
 import Navbar from "./components/Navbar";
+import Navigation from "./navbar/Navigation";
 // import Re5et from "./components/re5et";
 // import passwordRequirement from "./components/passwordRequirement";
 import Dashboard from"./pages/Dashboard"
@@ -18,67 +19,68 @@ import AddUser from"./pages/AddUser"
 import Visualize from"./pages/Visualize"
 import "./components/reset.css"
 import Testfetch from './pages/Testfetch';
-// import Navigation from "./navbar/Navigation"
 
 function App() {
-  // const [isLogin, SetIsLogin] = useState(false);
+  const [isLogin, SetIsLogin] = useState(false);
 
-  // useEffect(() => {
-  //   let token = localStorage.getItem('token');
-  //   if (token) {
-  //     SetIsLogin(true);
-  //   } else {
-  //     SetIsLogin(false);
-  //   }
-  // }, []);
+  useEffect(() => {
+    let token = localStorage.getItem('token');
+    if (token) {
+      SetIsLogin(true);
+    } else {
+      SetIsLogin(false);
+    }
+  }, []);
 
-  // const logout = () => {
-  //   localStorage.removeItem('token');
-  //   SetIsLogin(false)
-  // };
+  const logout = () => {
+    localStorage.removeItem('token');
+    SetIsLogin(false)
+  };
 
   return (
     <>
       <Router>
-        
-        {/* <div className="App"> */}
-      
-        <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route path='/gateway' element={<Gateway/>}/>
-          <Route path='/token' element={<Token/>}/>
-          <Route path='/adduser' element={<AddUser/>}/>
-          <Route exact path="/home" element={<LandingPage />} />
-          <Route path="/userDetails" element={<UserDetails />} />
-          <Route path="/reset" element={<Reset />} />
+        {isLogin ? (
+          <>
+          <Navbar />
+            <Routes>
+              <Route exact path="/" element={<LandingPage />} />
+              <Route path='/gateway' element={<Gateway/>}/>
+              <Route path='/token' element={<Token/>}/>
+              <Route path='/adduser' element={<AddUser/>}/>
+              <Route exact path="/home" element={<LandingPage />} />
+              <Route path="/userDetails" element={<UserDetails />} />
+              <Route path="/reset" element={<Reset />} />
+              <Route exact path='/dashboard' element={<Dashboard />} />
+              <Route path='/visualize' element={<Visualize/>}/>
+              <Route path='/testfetch' element={<Testfetch/>}/>
+              <Route exact path='/dashboard' element={<Dashboard />} />
+              <Route path='/visualize' element={<Visualize/>}/>
+              <Route path='/testfetch' element={<Testfetch/>}/>
+            </Routes>
+          </>
+        ) : (
+          <>
+            <Navigation />
+            <Routes>
+              <Route exact path="/" element={<LandingPage />} />
+              <Route exact path='/sign-up' element={<SignUp />} />
+              <Route exact path='/sign-in' element={<Login />} />
+            </Routes>
+          </>
+        )}
+        {/* <div>
+          <Navigation />
           <Route exact path='/sign-up' element={<SignUp />} />
           <Route exact path='/sign-in' element={<Login />} />
-        </Routes>
-        {/* <div className="auth-alternate"> */}
-          <Routes>
-            <Route exact path='/dashboard' element={<Dashboard />} />
-            <Route path='/visualize' element={<Visualize/>}/>
-            <Route path='/testfetch' element={<Testfetch/>}/>
-          </Routes>
-          {/* <div className="auth-wrapper">
-            <div className="auth-inner"> */}
-              <Routes>
-                <Route path='/navbar' element={<Navbar/>}/>
-                {/* <Route exact path="/" element={<LandingPage />} />
-                <Route exact path="/home" element={<LandingPage />} />
-                <Route path="/userDetails" element={<UserDetails />} />
-                <Route path="/reset" element={<Reset />} />
-                <Route exact path='/dashboard' element={<Dashboard />} />
-                <Route exact path='/sign-up' element={<SignUp />} />
-                <Route exact path='/sign-in' element={<Login />} />
-                <Route path='/visualize' element={<Visualize/>}/>
-                <Route path='/navbar' element={<Navbar/>}/> */}
-              </Routes>
-            {/* </div>
-          </div>
+          <Route exact path="/" element={<LandingPage />} />
         </div> */}
-        {/* </div> */}
       </Router>
+      {/* <Router>
+        <Routes>
+          
+        </Routes>
+      </Router> */}
     </>
   );
 }
