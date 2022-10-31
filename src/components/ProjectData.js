12db31b54a39c69bc9950147dfdf0b544f23edf6
thenardhis2021 <thenardhi2001@gmail.com>
2022-10-31 09:20:24 +0700

    update nama project

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const projectdata = [
     {
-    name: 'Kedaireka UMN',
+    name: 'Smartfarm UMN',
     url: 'https://drive.google.com/uc?export=view&id=1M-W-0a_OXLutzG5Io76AzcKSGMbBKXsw'
     }
 ]
