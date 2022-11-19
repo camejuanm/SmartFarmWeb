299dbbcf8bab9bc0aada3e20684f038dd9a8339f
thenardhis2021 <thenardhi2001@gmail.com>
2022-11-19 10:08:32 +0700

    membuat tampilan newUser dan status user apakah sudah terverifikasi atau belum dengan menggunakan table.

--- src/components/newUser.js
+++ src/components/newUser.js
@@ -9,29 +9,59 @@ export default class newUser extends Component {
             id: "",
             name: "",
             email: "",
+            status: "unverified",
             isVerified: false,
+            verify: false,
         };
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.verifyEmail = this.verifyEmail.bind(this);
+        this.sendEmail = this.sendEmail.bind(this);
     }
 
 
-    verifyEmail() {
-        if(this.state.id >=1 && this.state.name!== "undefined" && this.state.email!== "undefined") {
-            this.setState({ isVerified: true });
-        }   
+    verifyEmail(e) {
+        this.setState({ id: e.target.value, name: e.target.value, email: e.target.value }, function() {
+            if(this.state.id !== "undefined" && this.state.name !== "undefined" && this.state.email !== "undefined") {
+                this.setState({ isVerified: true });
+            }
+        });
+    }
+
+    sendEmail() {
+        alert("Email verified");
+        this.setState({
+            verify: true,
+            isVerified: false,
+            status: "verified",
+        });
     }
 
     handleSubmit(e) {
         e.preventDefault();
         const { id, name, email } = this.state;
         console.log(id, name, email);
-        if(this.state.isVerified) {
-            console.log("userVerify");
-            alert("Verification Done");
-            window.location.href="/sign-up";
+        if(this.state.verify) {
+            fetch("https://smart-farm-backend.vercel.app/api/user/all", {
+            method: "POST",
+            crossDomain: true,
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+                id,
+                name,
+                email,
+            }),
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data, "userVerify");
+                alert("Verification Done");
+                window.location.href="./sign-up";
+        });
         } else {
-            alert("User unverified");
+            alert("Please verify new user");
         }
     }
 
@@ -47,17 +77,26 @@ export default class newUser extends Component {
                                     <th id="id">Id</th>
                                     <th id="name">Name</th>
                                     <th id="email">Email</th>
-                                    <th id="isVerified">Verified</th>
+                                    <th id="status">Status</th>
+                                    <th id="isVerified">Verification Status</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr onChange={(e) => this.verifyEmail(e)}>
+                                <tr>
+                                    <td>633e9f56e1b073cf48d9f81c</td>
+                                    <td>Anakin</td>
+                                    <td>starwars@example.com</td>
+                                    <td>verified</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
                                     <td className="id">
                                         <input
-                                            type="number"
+                                            type="text"
                                             id="id"
                                             className="form-control"
-                                            onChange={(e) => this.setState({ id: e.target.value })}
+                                            onChange={(e) => {this.verifyEmail(e)}}
+                                            unique
                                         />
                                     </td>
                                     <td className="name">
@@ -65,7 +104,7 @@ export default class newUser extends Component {
                                             type="text"
                                             id="name"
                                             className="form-control"
-                                            onChange={(e) => this.setState({ name: e.target.value })}
+                                            onChange={(e) => {this.verifyEmail(e)}}
                                         />
                                     </td>
                                     <td className="email">
@@ -73,15 +112,17 @@ export default class newUser extends Component {
                                             type="email"
                                             id="email"
                                             className="form-control"
-                                            onChange={(e) => this.setState({ email: e.target.value })}
+                                            onChange={(e) => {this.verifyEmail(e)}}
                                         />
                                     </td>
+                                    <td className="status">{this.state.status}</td>
                                     <td className="isVerified">
                                         {this.state.isVerified? (
                                             <input
                                                 type="button"
-                                                value="verify"
+                                                value={this.state.verify ? "verified" : "verify"}
                                                 className="form-control"
+                                                onClick={this.sendEmail}
                                                 style={{
                                                     backgroundColor: "blue",
                                                     width: "100%",
@@ -98,10 +139,10 @@ export default class newUser extends Component {
                     </div>
                     <div className="d-grid">
                     <button
-                        type="submit" 
+                        type="submit"
                         className="btn btn-success"
                     >
-                        Sign Up
+                        Verify User
                     </button>
                 </div>
                 </form>
