558df5988e7179f1480b93abd2de94ac2ca21007
thenardhis2021 <thenardhi2001@gmail.com>
2022-11-23 11:07:34 +0700

    mengubah halaman signup dan halaman newUser. Untuk halaman newUser membuat filter dan mapping untuk menampilkan daftar role user yang belum terverifikasi.

--- src/components/newUser.js
+++ src/components/newUser.js
@@ -10,12 +10,7 @@ import {SidebarDataAdmin} from './SidebarDataAdmin';
 import { SidebarDataUser } from './SidebarDataUser';
 import { SidebarDataLanding } from './SidebarDataLanding.';
 import { IconContext} from 'react-icons'
-import { Outlet } from 'react-router-dom';
-import { SidebarDataVerification } from './SidebarDataVerification';
 import { isCompositeComponent } from 'react-dom/test-utils';
-
-// const auth = window.localStorage.getItem("isAuth");
-// const admin = window.localStorage.getItem("isAdmin")
 
 export default class newUser extends Component {
     constructor(props) {
@@ -24,20 +19,35 @@ export default class newUser extends Component {
             id: "",
             name: "",
             email: "",
-            status: "unverified",
             isVerified: false,
-            datas: "",
+            datas: [],
             verify: false,
             verifyButton: true,
+            updateVerification: true,
+            history: false,
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.sendEmail = this.sendEmail.bind(this);
         this.updateVerification = this.updateVerification.bind(this);
+        this.verification = this.verification.bind(this);
     }
 
     updateVerification() {
         console.log("Verification updated");
-        this.setState({ isVerified: true, verifyButton: false });
+        alert("All users verified");
+        this.setState({
+            isVerified: true,
+            updateVerification: false,
+            history: true
+        });
+    }
+
+    verification() {
+        console.log("User verify");
+        this.setState({
+            isVerified: true,
+            verifyButton: false
+        })
     }
     verifyEmail(e) {
         this.setState({
@@ -55,7 +65,6 @@ export default class newUser extends Component {
         this.setState({
             verify: true,
             isVerified: false,
-            status: "verified",
         });
     }
 
@@ -72,7 +81,9 @@ export default class newUser extends Component {
         })
         .then((res) => res.json())
         .then((data) => {    
-            this.setState({ datas : data })
+            this.setState({ datas : data.filter((item) => {
+                return item.isVerified == false && item.role == 'user';
+            })})
             console.log(data);
         });
     }
@@ -83,12 +94,12 @@ export default class newUser extends Component {
 
     displayData(datas){
         let table = '<table border="1">';
-        table += `<tr><th>ID</th><th>Name</th><th>Rank</th></tr>`;
+        table += `<tr><th>Name</th><th>Email</th><th>Id</th></tr>`;
         datas.forEach((datas, index) => {
             table = table + `<tr>`;
-            table = table + `<td>Title: ${datas.name}</td>`;
-            table = table + `<td>Title: ${datas.email}</td>`;
-            table = table + `<td>Title: ${datas.id}</td>`;
+            table = table + `<td>Name: ${datas.name}</td>`;
+            table = table + `<td>Email: ${datas.email}</td>`;
+            table = table + `<td>ID: ${datas.id}</td>`;
             table += `</tr>`;
          });
          table += "</table>";
@@ -109,139 +120,63 @@ export default class newUser extends Component {
             <div className="outer">
                 <form onSubmit={this.handleSubmit}>
                     <h3>Verification User History</h3>
+                    <>
+                    {this.state.history ? (
+                        <h2 className="history">No New User which is unverified</h2>
+                    ):null}
+                    </>
+                    {this.state.updateVerification ? (
+                    <>
                     <div className="table">
                         <table>
                             <thead>
                                 <tr>
                                     <th id="name">Name</th>
                                     <th id="email">Email</th>
-                                    <th id="id">Verification Status</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>    
-                                    <td className="name">
-                                        <BiIcons.BiRename />
-                                        {this.state.name}
-                                    </td>
-                                    <td className="email">
-                                        <AiIcons.AiOutlineMail />
-                                        {this.state.email}
-                                    </td>
-                                    <td>
-                                        {this.state.verifyButton ? (
-                                        <input
-                                            type="button"
-                                            value="verify"
-                                            onClick={this.updateVerification}
-                                        />
-                                        ):null}
-                                    </td>
-                                    
-                                </tr>
-                            </tbody>
-                        {/* <IconContext.Provider value={{color:'#fff'}}>
-                            <div className='navbar'>
-                                <Link to='#' className='menu-bars'>
-                                <FaIcons.FaBars onClick={this.showSidebar}/>
-                                </Link>
-                            </div>
-                            <nav className={this.sidebar ? 'nav-menu active' : 'nav-menu'}>
-                                <ul className='nav-menu-items' onClick={this.showSidebar}>
-                                    <li className='navbar-toggle'>
-                                        <Link to="#" className='menu-bars'>
-                                            <AiIcons.AiOutlineClose />
-                                        </Link>
-                                    </li>
-                                    {this.SidebarDataVerification.map((datas, index) => {
-                                        return (
-                                            <li key={index} className={datas.cName}>
-                                                <Link to={datas.path}>
-                                                    {datas.icon}
-                                                    <span>{datas.title}</span>
-                                                </Link>
-                                            </li>
-                                        )
-                                    })}
-                                    <li className='nav-logout' onClick={() => {window.localStorage.clear(); window.location.reload()}}><Link to="/"><AiIcons.AiOutlineLogout/><span>Logout</span></Link></li>
-                                </ul>
-                            </nav>
-                        </IconContext.Provider>
-                        <Outlet/> */}
-                            {/* <thead>
-                                <tr>
-                                    <th id="id">Id</th>
-                                    <th id="name">Name</th>
-                                    <th id="email">Email</th>
-                                    <th id="status">Status</th>
                                     <th id="isVerified">Verification Status</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>633e9f56e1b073cf48d9f81c</td>
-                                    <td>Anakin</td>
-                                    <td>starwars@example.com</td>
-                                    <td>verified</td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td className="id">
-                                        <input
-                                            type="text"
-                                            id="id"
-                                            className="form-control"
-                                            onChange={(e) => this.setState({ id: e.target.value })}
-                                            unique
-                                        />
-                                    </td>
-                                    <td className="name">
-                                        <input
-                                            type="text"
-                                            id="name"
-                                            className="form-control"
-                                            onChange={(e) => {this.verifyEmail(e)}}
-                                        />
-                                    </td>
-                                    <td className="email">
-                                        <input
-                                            type="email"
-                                            id="email"
-                                            className="form-control"
-                                            onChange={(e) => {this.verifyEmail(e)}}
-                                        />
-                                    </td>
-                                    <td className="status">{this.state.status}</td>
-                                    <td className="isVerified">
-                                        {this.state.isVerified? (
-                                            <input
-                                                type="button"
-                                                value={this.state.verify ? "verified" : "verify"}
-                                                className="form-control"
-                                                onClick={this.sendEmail}
-                                                style={{
-                                                    backgroundColor: "#4040db",
-                                                    width: "100%",
-                                                    padding: 5,
-                                                    color: "white",
-                                                    border:"none",
-                                                }}
-                                            />
-                                        ):null}
-                                    </td>
-                                </tr>
-                            </tbody> */}
+                                <>
+                                    {this.state.datas.map((item, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td className="name">{item.name}</td>
+                                                <td className="email">{item.email}</td>
+                                                {this.state.verifyButton ? ( 
+                                                <td className="isVerified">
+                                                    <input
+                                                        type="button"
+                                                        value="verify"
+                                                        // onClick={this.verification}
+                                                        style={{
+                                                            backgroundColor: "blue",
+                                                            width: "100%",
+                                                            padding: 4,
+                                                            color: "white",
+                                                            border:"none",
+                                                        }}
+                                                    />
+                                                </td>
+                                                ):null}
+                                            </tr>
+                                        )
+                                    })}
+                                </>
+                            </tbody>
                         </table>
                     </div>
-                    <div className="d-grid">
-                    <button
-                        type="submit"
-                        className="btn btn-success"
-                        onClick={this.updateVerification}
-                    >
-                        Verify User
-                    </button>
-                </div>
+                    {/* <div className="d-grid">
+                        <button
+                            type="submit"
+                            className="btn btn-success"
+                            onClick={this.updateVerification}
+                        >
+                            Verify All User
+                        </button>
+                    </div> */}
+                    </>
+                    ):null}
                 </form>
             </div>
         );
